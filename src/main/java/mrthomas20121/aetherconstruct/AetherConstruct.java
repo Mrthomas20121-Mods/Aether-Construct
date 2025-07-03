77d277a9dd010ca7c9c2058a66329b0b3df964bd
Thomas B <thomas77300@gmail.com>
2025-07-03 18:28:27 +0200

    Create AetherConstruct.java

---
+++ src/main/java/mrthomas20121/aetherconstruct/AetherConstruct.java
@@ -0,0 +1,47 @@
+package mrthomas20121.aetherconstruct;
+
+import mrthomas20121.aetherconstruct.datagen.ThermalConstructDatagen;
+import mrthomas20121.aetherconstruct.init.AetherConstructFluids;
+import mrthomas20121.aetherconstruct.init.AetherConstructItems;
+import mrthomas20121.aetherconstruct.init.AetherConstructModifiers;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegisterEvent;
+
+@Mod(AetherConstruct.MOD_ID)
+public class AetherConstruct {
+
+	public static final String MOD_ID = "aetherconstruct";
+
+	public static String makeDescriptionId(String type, String name) {
+		return type + "." + MOD_ID + "." + name;
+	}
+
+	public static ResourceLocation getResource(String name) {
+		return new ResourceLocation(MOD_ID, name);
+	}
+
+	public AetherConstruct() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		AetherConstructItems.ITEMS.register(bus);
+		AetherConstructItems.CREATIVE_TABS.register(bus);
+		AetherConstructFluids.FLUIDS.register(bus);
+		AetherConstructModifiers.MODIFIERS.register(bus);
+
+		bus.addListener(EventPriority.NORMAL, false, GatherDataEvent.class, ThermalConstructDatagen::init);
+		bus.addListener(EventPriority.NORMAL, false, RegisterEvent.class, this::register);
+	}
+
+	private void register(RegisterEvent event) {
+		if(event.getRegistryKey() == Registries.RECIPE_SERIALIZER) {
+			// register entity predicate
+
+		}
+	}
+}
